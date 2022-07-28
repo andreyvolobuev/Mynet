a957bc4fa73df2ed5ea1580fb24efe29d97e1b23
Andrey Volobuev <avvolob@gmail.com>
2022-07-28 23:20:32 +0300

    Refactored loss functions

--- mynet/loss.py
+++ mynet/loss.py
@@ -1,28 +1,9 @@
-import math
-from utils import debatch
+def MSELoss(y_target, y_pred):
+    result = 0
+    for i, j in zip(y_target, y_pred):
+        result += (i - j) ** 2
+    return result / len(y_target)
 
-
-class MSELoss:
-    pass
-
-
-
-@debatch
-def CrossEntropyLoss(sample):
-    y_target, y_pred = sample
-    if len(y_target) != len(y_pred):
-        raise ValueError(
-            "Target vector length (%s) has to be same with "
-            "predicted distribution vector length (%s)"
-            % (len(y_target), len(y_pred))
-        )
-    if not math.isclose(sum(y_pred), 1):
-        raise ValueError(
-            "Predicted probability distribution has to sum up to 1 (100%)"
-        )
-    if sum(y_target) != 1:
-        raise ValueError(
-            "Target vector values have to sum up to 1"
-        )
+def CrossEntropyLoss(y_target, y_pred):
     y = y_target.index(max(y_target))
-    return -math.log(y_pred[y] or 1e-100)
+    return y_pred[y].log() * -1
