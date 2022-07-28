03e1df4f281fb6fe17b9b504f149626e1223957a
Andrey Volobuev <avvolob@gmail.com>
2022-07-28 16:40:19 +0300

    Refactored neuron

--- mynet/neuron.py
+++ mynet/neuron.py
@@ -1,28 +1,26 @@
+from value import Value
 import random
-from activation import ReLU, Softmax
-from utils import debatch
-
 
 class Neuron:
     def __init__(self, n_inputs=None, weights=None, bias=None):
-        self.bias = bias or random.uniform(-1, 1)
-        self.weights = weights or [random.uniform(-1, 1) for i in range(n_inputs)]
-        if n_inputs and len(self.weights) != n_inputs:
-            raise ValueError(
-                "Length of weights (%s) has to be equal "
-                "to number of inputs. (%s) "
-                % (n_inputs, len(self.weights))
-            )
+        self.weights = weights or [Value(random.uniform(-1, 1)) for i in range(n_inputs)]
+        self.bias = bias or Value(random.uniform(-1, 1))
 
-    @debatch
+    def parameters(self):
+        return self.weights + [self.bias]
+        
     def forward(self, X):
         if len(X) != len(self.weights):
-            raise ValueError(
-                "Length of input data (%s) has to be equal "
-                "to length of the neuron's weights (%s) "
+            raise TypeError(
+                "Length of the input data (%s) has to be equal "
+                "to the length of the neuron's weights (%s) "
                 % (len(X), len(self.weights))
             )
         result = self.bias
         for i, j in zip(self.weights, X):
-            result += i*j
+            result += i * j
         return result
+
+    @classmethod
+    def new(cls, weights, bias):
+        return cls(weights=[Value(w) for w in weights], bias=Value(bias))
