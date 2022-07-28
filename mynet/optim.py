e19f114a294bcd65fc72f127f76d185f9cad8c42
Andrey Volobuev <avvolob@gmail.com>
2022-07-28 23:10:24 +0300

    Created optimizer

---
+++ mynet/optim.py
@@ -0,0 +1,12 @@
+class Optim:
+	def __init__(self, parameters=parameters, lr=1):
+		self.parameters = parameters
+		self.lr = lr
+
+	def zero_grad(self):
+		for parameter in self.parameters:
+			parameter.grad = None
+
+	def step(self):
+		for parameter in self.parameters:
+			parameter.data -= parameter.grad.data * self.lr
