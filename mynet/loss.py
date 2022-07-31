45e2c92557305b73bf3e8ae516da37ac4f6a1be4
Andrey Volobuev <avvolob@gmail.com>
2022-07-31 15:17:53 +0300

    Renamed variables in MSELoss

--- mynet/loss.py
+++ mynet/loss.py
@@ -1,6 +1,6 @@
-def MSELoss(y_target, y_pred):
+def MSELoss(targets, predictions):
     sum_sq_err, count_operations = 0, 0
-    for target, pred in zip(y_target, y_pred):
+    for target, pred in zip(targets, predictions):
         for t, p in zip(target, pred):
             sum_sq_err += (t - p) ** 2
             count_operations += 1
