f38fbc66b8174545bbf153a215b97af9b993d78b
Andrey Volobuev <avvolob@gmail.com>
2022-08-04 00:38:20 +0300

    Refactored loss function for persistance in var naming

--- mynet/loss.py
+++ mynet/loss.py
@@ -1,10 +1,10 @@
 def mse_loss(targets, predictions):
-    sum_sq_err, count_operations = 0, 0
+    sum_sq_err, n_operations = 0, 0
     for target, pred in zip(targets, predictions):
         for t, p in zip(target, pred):
             sum_sq_err += (t - p) ** 2
-            count_operations += 1
-    return sum_sq_err / count_operations
+            n_operations += 1
+    return sum_sq_err / n_operations
 
 
 def cross_entropy_loss(targets, predictions, epsilon=1e-10):
