a4a64b131d14d114c21547a585a126fca326693f
Andrey Volobuev <avvolob@gmail.com>
2022-08-04 17:56:25 +0300

    Removed one line from cross entropy loss

--- mynet/loss.py
+++ mynet/loss.py
@@ -11,9 +11,8 @@ def mse_loss(targets, predictions):
 
 
 def cross_entropy_loss(targets, predictions, epsilon=1e-10):
-    log_sum, n_operations = 0, 0
+    log_sum = 0
     for target, pred in zip(targets, predictions):
         target_idx = target.index(max(target))
         log_sum += (pred[target_idx] + epsilon).log()
-        n_operations += 1
-    return -log_sum / n_operations
+    return -log_sum / len(targets)
